110697adb3927c447223dbc2185c5188d8953b66
Dudekula Baba Fakruddin <fakruddinbaba.ai@gmail.com>
2025-10-07 20:17:37 +0530

    created multi chat application and replaced ai response with custom response for building purpose

--- frontend.py
+++ frontend.py
@@ -1,14 +1,72 @@
 import streamlit as st
 from main import chat
 from langchain_core.messages import HumanMessage
+import uuid
+
+# helper to generate new thread_id
+def generate_thread_id():
+    return str(uuid.uuid4())
+
+
+
+
+def clean_chat_history():
+    st.session_state['messages']=[]
+
+def load_conversation(thread_id):
+    state = chat.get_state(config={'configurable': {'thread_id': thread_id}})
+    # Check if messages key exists in state values, return empty list if not
+    return state.values.get('messages', [])
+
+
+# thread_id stores current thread_id if not create new one
+if 'thread_id' not in st.session_state:
+    thread_id=generate_thread_id()
+    # create a obj form with thread_id and Name of the Chat for now and later it stored in history with new name
+    st.session_state['thread_id']={'thread_id':thread_id,'name':thread_id[:10]}
+
+# thread_history storing history of threads objects with id and name
+if 'threads_history' not in st.session_state:
+    st.session_state['threads_history']=[st.session_state['thread_id']]
+
+
+
 
 if 'messages' not in st.session_state:
     st.session_state['messages']=[]
+
+
+
+
+st.sidebar.title("ChatBot")
+
+if st.sidebar.button("New Chat"):
+    clean_chat_history()
+    thread_id=generate_thread_id()
+    st.session_state['thread_id']={'thread_id':thread_id,'name':thread_id[:10]}
+    st.session_state['threads_history'].append(st.session_state['thread_id'])
+
+for thread in st.session_state['threads_history']:
+    if st.sidebar.button(thread['name']):
+        clean_chat_history()
+        st.session_state['thread_id']={'thread_id':thread['thread_id'],'name':thread['name']}
+        messages=load_conversation(thread_id=thread['thread_id'])
+        for message in messages:
+            st.session_state['messages'].append(
+                {
+                    'role':'user' if isinstance(message, HumanMessage) else 'assistant',
+                    'content':message.content
+                }
+            )
+
+
 
 for message in st.session_state['messages']:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
 
+
+CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']['thread_id']}}
 user_input = st.chat_input()
 
 
@@ -30,7 +88,7 @@ if user_input:
             {'messages': [HumanMessage(content=user_input)]},
             
             # Config: thread_id keeps conversation state across turns
-            # config={'configurable': {'thread_id': 'thread-1'}},
+            config=CONFIG,
             
             # Stream mode: stream full messages instead of raw tokens
             stream_mode='messages'
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from langgraph.graph import StateGraph,START,END,MessagesState
 from model import llm
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage,AIMessageChunk
+from langgraph.checkpoint.memory import InMemorySaver
 
 def llm_node(state:MessagesState):
     query=state['messages']
-    response=llm.invoke(query)
+    response=AIMessageChunk(content="llm.invoke(query)")
     return {
         'messages':response
     }
+
+
+checkpoint=InMemorySaver()
 
 graph=StateGraph(MessagesState)
 graph.add_node("llm_node",llm_node)
 graph.add_edge(START,'llm_node')
 graph.add_edge('llm_node',END)
-chat=graph.compile()
+chat=graph.compile(checkpointer=checkpoint)
