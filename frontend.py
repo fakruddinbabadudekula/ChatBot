d2bef8f2982ef20975e385f101576d6c8f73a4b5
Dudekula Baba Fakruddin <fakruddinbaba.ai@gmail.com>
2025-10-07 22:53:57 +0530

    Used SQL Lite as a checkpoint

--- frontend.py
+++ frontend.py
@@ -2,17 +2,28 @@ import streamlit as st
 from main import chat
 from langchain_core.messages import HumanMessage
 import uuid
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+
+DATA_BASE="checkpoints.db"# Data Base file Name
 
 # helper to generate new thread_id
 def generate_thread_id():
     return str(uuid.uuid4())
 
 
+# return list of threads which are currently stored
+def retrieve_all_threads(checkpoint):
+    all_threads = set()
+    for checkpoint in checkpoint.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
 
+    return list(all_threads)
 
+# clearing the chat sessions
 def clean_chat_history():
     st.session_state['messages']=[]
-
+# Loading the Chat history from the specified thred_id
 def load_conversation(thread_id):
     state = chat.get_state(config={'configurable': {'thread_id': thread_id}})
     # Check if messages key exists in state values, return empty list if not
@@ -23,15 +34,26 @@ def load_conversation(thread_id):
 if 'thread_id' not in st.session_state:
     thread_id=generate_thread_id()
     # create a obj form with thread_id and Name of the Chat for now and later it stored in history with new name
-    st.session_state['thread_id']={'thread_id':thread_id,'name':thread_id[:10]}
+    st.session_state['thread_id']=thread_id
+
+# chekpoint for sql to access the stored history
+if 'checkpoint' not in st.session_state:
+    conn=sqlite3.connect(DATA_BASE,check_same_thread=False)
+    checkpoint=SqliteSaver(conn=conn)
+    st.session_state['checkpoint']=checkpoint
+
 
 # thread_history storing history of threads objects with id and name
 if 'threads_history' not in st.session_state:
-    st.session_state['threads_history']=[st.session_state['thread_id']]
+    st.session_state['threads_history']=[st.session_state['thread_id']]+retrieve_all_threads(checkpoint=st.session_state['checkpoint'])
+    # For sql retrieve all threads 
+    # graph automatically fetch the history of messages from the sql when we pass the thread id it like inmemory checkpointer
+    # st.session_state['threads_history']=st.session_state['threads_history'].append(retrieve_all_threads(checkpoint=st.session_state['checkpoint']))
 
 
 
 
+# history of messages for a specified thread_id
 if 'messages' not in st.session_state:
     st.session_state['messages']=[]
 
@@ -40,17 +62,18 @@ if 'messages' not in st.session_state:
 
 st.sidebar.title("ChatBot")
 
+# Creating the New Chat
 if st.sidebar.button("New Chat"):
-    clean_chat_history()
+    clean_chat_history()#clearning history of messages
     thread_id=generate_thread_id()
-    st.session_state['thread_id']={'thread_id':thread_id,'name':thread_id[:10]}
+    st.session_state['thread_id']=thread_id
     st.session_state['threads_history'].append(st.session_state['thread_id'])
 
-for thread in st.session_state['threads_history']:
-    if st.sidebar.button(thread['name']):
+for thread_id in st.session_state['threads_history']:
+    if st.sidebar.button(thread_id):
         clean_chat_history()
-        st.session_state['thread_id']={'thread_id':thread['thread_id'],'name':thread['name']}
-        messages=load_conversation(thread_id=thread['thread_id'])
+        st.session_state['thread_id']=thread_id
+        messages=load_conversation(thread_id=thread_id)
         for message in messages:
             st.session_state['messages'].append(
                 {
@@ -66,7 +89,7 @@ for message in st.session_state['messages']:
         st.markdown(message['content'])
 
 
-CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']['thread_id']}}
+CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
 user_input = st.chat_input()
 
 
--- main.py
+++ main.py
@@ -2,19 +2,29 @@ from langgraph.graph import StateGraph,START,END,MessagesState
 from model import llm
 from langchain_core.messages import HumanMessage,AIMessageChunk
 from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+
+# Create SQLite connection
+# sqlite3.ProgrammingError: SQLite objects created in a thread can only be used in that same thread. The object was created in thread id 8948 and this is thread id 3776.
+# so pass the check_same_thread=False
+sql_connector = sqlite3.connect("checkpoints.db",check_same_thread=False)
 
 def llm_node(state:MessagesState):
     query=state['messages']
-    response=AIMessageChunk(content="llm.invoke(query)")
+    response=llm.invoke(query)
     return {
         'messages':response
     }
 
 
-checkpoint=InMemorySaver()
+# checkpoint=InMemorySaver()
+
+checkpoint=SqliteSaver(conn=sql_connector)
 
 graph=StateGraph(MessagesState)
 graph.add_node("llm_node",llm_node)
 graph.add_edge(START,'llm_node')
 graph.add_edge('llm_node',END)
 chat=graph.compile(checkpointer=checkpoint)
+
